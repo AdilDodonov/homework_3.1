7b4f0dfb967fb5c5116be0a8b87b467ce756c3d2
AdilDodonov <115130286+AdilDodonov@users.noreply.github.com>
2022-10-26 21:27:18 +0600

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String names[] = {"Azamat", "bakyt", "tor"};
+        for (String name : names) {
+            switch (name){
+                case "Azamat":
+                    System.out.println();
+
+
+            }
+        }
+
+
+    }
+}
